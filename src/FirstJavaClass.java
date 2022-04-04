363dcb44b1f20b24851b4d3b40754e8eddd5a8a2
Hansa Vera Celikaslan <atomkarinca2b@gmail.com>
2022-04-04 13:18:54 +0200

    I made some changes

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,6 +1,7 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("learning github");
+        System.out.println("adding new code");
+        System.out.println("This is another code");
     }
 }
-g'g
